42dcef0451274a25fca4d0db40f437231a42218b
Jeevan <103121525+j-e-e-v-a-n@users.noreply.github.com>
2025-03-10 15:13:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     UPI_ID: "your-upi@bank",         // Add your actual UPI ID
     BUSINESS_NAME: "Your Store",      // Add your actual business name
     PAYMENT_QR_PATH: "./payment-qr.jpg",  // Make sure to add your UPI QR image with this name
-    WEB_USERNAME: "admin",           // Username for web interface
-    WEB_PASSWORD: "yourpassword123", // Password for web interface
+    WEB_USERNAME: "faris",           // Username for web interface
+    WEB_PASSWORD: "1234", // Password for web interface
     PORT: 3000                       // Port for web interface
 };
