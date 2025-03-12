c9a2df4ea1d134edd92215d2cdad2d5bd489a8fa
Jeevan <103121525+j-e-e-v-a-n@users.noreply.github.com>
2025-03-12 20:07:22 +0530

    ,

--- dbb/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    WEB_PASSWORD: 'password123',           // Password for web interface (optional use)
-    PORT: process.env.PORT || 5000,        // Server port for web interface / socket.io
-  };
-
-  export default config;
